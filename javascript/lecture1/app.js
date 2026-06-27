96efe7850d80be34ae2a9e84ae326e43a60642b7
Amar kumar <amar766730@gmail.com>
2026-06-27 12:16:53 +0530

    variable-completed

---
+++ javascript/lecture1/app.js
@@ -0,0 +1,33 @@
+//esme var ko reinstiilize kr skte hai
+// redeclare kr skte hai
+var b = 10;
+var b = 20;
+
+//reassignment is allowed
+var city = "giridih"
+    city = "hazaribagh"
+
+//function scope
+function test(){
+    var x = 10;
+}
+//console.log(x); // x not defined
+
+
+
+// esme reinstilize nhi kr skte hai
+//reassignment is not allowed
+let a =10;
+ a=20;
+console.log(a);
+console.log(a);
+
+//without using datatype we can use but not recommended
+p=10;
+console.log(p)
+
+console.log(num);//undefined dega
+var num = 45;
+
+console.log(n); //give error cannot access n before intilization
+let n = 45;
