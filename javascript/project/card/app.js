37442c266c0ae1f885adf706eeabdacc060d3189
Amar kumar <amar766730@gmail.com>
2026-07-24 21:52:30 +0530

    dom-traversal

---
+++ javascript/project/card/app.js
@@ -0,0 +1,46 @@
+const container = document.getElementById("container")
+const result = document.getElementById("result");
+
+//update count
+function updateCount(){
+    result.inner = "Total Student :"+container.children.length;
+}updateCount();
+
+// delete count
+const deleteBtn = document.querySelectorAll(".deleteBtn")
+for(let button of deleteBtn){
+    button.addEventListener("click",function(){
+        this.closest(".card").remove();
+        updateCount();
+    })
+}
+ const favCard = document.querySelectorAll(".favoriteBtn");
+ for(let favr of favCard){
+favr.addEventListener("click",function(){
+    this.closest("card").classList.add("fav")
+})
+ }
+
+ const prevBtn = document.querySelectorAll(".prevBtn");
+
+for (let button of prevBtn) {
+  button.addEventListener("click", function () {
+    let currCard = this.closest(".card");
+    let prevCard = currCard.previousElementSibling;
+
+    // Fixed: Changed nextCard to prevCard
+    if (prevCard) {
+      alert("Previous Student : " + prevCard.children[0].innerText);
+    } else {
+      alert("No previous student");
+    }
+  });
+}
+
+const firsBtn = document.getElementById("firstBtn");
+
+firsBtn.addEventListener("click", function () {
+  let firstStudent = container.firstElementChild;
+  alert(firstStudent.childer[0].innerText)
+});
+
